99f3d2b1e7135f76670aedb696fb3658d0a8ad84
linusgg <15870909+linusgg@users.noreply.github.com>
2019-10-03 19:52:20 +1000

    add class

--- sources/calc.py
+++ sources/calc.py
@@ -26,3 +26,34 @@ def add2(arg1, arg2):
         arg1conv = str(arg1conv)
         arg2conv = str(arg2conv)
     return arg1conv + arg2conv
+import feedparser
+import markdown
+import jinja2
+import requests
+import yaml
+
+
+class Preprocessors:
+    """
+    Built-in context preprocessors.
+    Context preprocessors are functions that receive the context used to
+    render the templates, and enriches it with additional information.
+    The original context is obtained by parsing ``config.yml``, and
+    anything else needed just be added with context preprocessors.
+    """
+
+    @staticmethod
+    def navbar_add_info(context):
+        """
+        Items in the main navigation bar can be direct links, or dropdowns with
+        subitems. This context preprocessor adds a boolean field
+        ``has_subitems`` that tells which one of them every element is. It
+        also adds a ``slug`` field to be used as a CSS id.
+        """
+        for i, item in enumerate(context["navbar"]):
+            context["navbar"][i] = dict(
+                item,
+                has_subitems=isinstance(item["target"], list),
+                slug=(item["name"].replace(" ", "-").lower()),
+            )
+        return context
